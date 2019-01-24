b75d7164e685de4a0784b3b4a6b7d347e477250d
angeljuarez77 <angeljuarez729@gmail.com>
2019-01-24 13:47:57 -0500

    started oop style js slideshow. got images to hide

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const slideshow = {
+  images: document.getElementsByClassName('slider-image-wrapper'),
+  hideImages: function(){
+    const images = [...document.getElementsByClassName('slider-image-wrapper')];
+    images.forEach(image => image.style.display = 'none')
+  },
+  addingListener: function(){
+    const slideshow = document.getElementsByClassName('slideshow')[0];
+    slideshow.addEventListener('click', this.sliderFunction);
+  },
+  sliderFunction: function(e){
+     let imageNumber = 0;
+    if(e.target.id === 'rightArrow'){
+      imageNumber++;
+      const image = this.images[imageNumber];
+      image.style.display = 'block';
+    }
+    if(e.target.id === 'leftArrow'){
+      imageNumber--;
+      const image = this.images[imageNumber];
+      image.style.display = 'block';
+    }
+  }
+}
+slideshow.hideImages();
+slideshow.addingListener();
