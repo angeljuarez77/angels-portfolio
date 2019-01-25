b4c20f9f0f456f83b3064140b90dac3eaf4dba98
angeljuarez77 <angeljuarez729@gmail.com>
2019-01-25 04:25:22 -0500

    tried to set slide number to local storage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
+// first tryyyyyyy! Time to start learning Object Oriented Programming
+document.onload(() => localStorage.setItem('slide-number', 0));
+
 const slideshow = {
   images: document.getElementsByClassName('slider-image-wrapper'),
   hideImages: function(){
+    // spread operator used because by default they are dom nodes and I had to convert them from an array of dom nodes to an iterable one
     [...this.images].forEach(image => image.style.display = 'none');
   },
   addingListener: function(){
     const slideshow = document.getElementsByClassName('slideshow')[0];
     slideshow.addEventListener('click', this.arrowFunction.bind(this));
   },
+  initializeSlideNum: function(){
+    localStorage.setItem('slide-number', 0);
+  },
   arrowFunction: function(e){
-    let imageNumber = 0;
+    let imageNumber = localStorage.getItem('slide-number');
     if(e.target.id === 'rightArrow'){
+      imageNumber++;
+      localStorage.setItem('slide-number', imageNumber);
+      console.log(imageNumber);
       const image = this.images[imageNumber];
       image.style.display = 'block';
     }
@@ -19,10 +29,6 @@ const slideshow = {
       image.style.display = 'block';
     }
   },
-  testing: function(e){
-    console.log(this.images);
-  }
 }
 slideshow.hideImages();
-slideshow.testing();
 slideshow.addingListener();
